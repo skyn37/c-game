22efda0ed1e9e59fb1631cb4499d9fc2638edfb4
skyn37 <name@noname.com>
2024-01-03 02:00:59 +0200

    build and run script

---
+++ build_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script to build and run the project
+
+# Run 'make' in the 'build/' directory
+make -C build/
+
+# Run the executable from the 'build/bin/' directory
+./build/bin/GameProject
+
