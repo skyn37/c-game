3d1946bd8ed2faf19cdb98725b7d53901fdb5bd9
skyn37 <name@noname.com>
2024-01-05 00:02:31 +0200

    grid based collision detection .. better than doubler for Ls

--- controls.c
+++ controls.c
@@ -1,20 +1,55 @@
 #include "raylib.h"
 #include <stdio.h>
+#include <game.h>
+#include <math.h>
 
 
-int SPEEDx = 100;
-int SPEEDy = 100;
+// TODO sexy globals
+int SPEEDx = 10;
+int SPEEDy = 10;
 
 
-void HandlePlayerControls(Vector2* Player)
-{
-    if (IsKeyDown(KEY_RIGHT)) Player->x += 100.0f * GetFrameTime() * SPEEDx;
-    if (IsKeyDown(KEY_LEFT)) Player->x -= 100.0f * GetFrameTime() * SPEEDx;
-    if (IsKeyDown(KEY_UP)) Player->y -= 100.0f * GetFrameTime() * SPEEDy;
-    if (IsKeyDown(KEY_DOWN)) Player->y += 100.0f * GetFrameTime() * SPEEDy;
+
+// TODO This is the ground texture w and h should handle that better
+#define GRID_CELL_SIZE 600
+// Grid based collision detection .. its ok  TODO:  needs improvement 
+// Function to convert world coordinates to grid coordinates
+void WorldToGrid(Vector2 worldPos, int* gridX, int* gridY) {
+    *gridX = (int)(worldPos.x / GRID_CELL_SIZE);
+    *gridY = (int)(worldPos.y / GRID_CELL_SIZE);
 }
 
-void ControlsInitControls(Vector2* Player)
-{
-    HandlePlayerControls(Player);
+// Function to check if a cell in the grid contains an obstacle
+bool CellContainsObstacle(GridData* mapData, int gridX, int gridY) {
+    if (gridX >= 0 && gridX < mapData->width && gridY >= 0 && gridY < mapData->height) {
+        return mapData->grid[gridX + gridY * mapData->width].textureInfo == 0;
+    }
+    return false;
 }
+
+void HandlePlayerControls(Vector2* Player, GridData* mapData) {
+    Vector2 nextPosition = *Player;
+    if (IsKeyDown(KEY_RIGHT)) nextPosition.x += 100.0f * GetFrameTime() * SPEEDx;
+    if (IsKeyDown(KEY_LEFT)) nextPosition.x -= 100.0f * GetFrameTime() * SPEEDx;
+    if (IsKeyDown(KEY_UP)) nextPosition.y -= 100.0f * GetFrameTime() * SPEEDy;
+    if (IsKeyDown(KEY_DOWN)) nextPosition.y += 100.0f * GetFrameTime() * SPEEDy;
+
+    // Convert next position to grid coordinates
+    int gridX, gridY;
+    WorldToGrid(nextPosition, &gridX, &gridY);
+
+    // Check if the grid cell contains an obstacle
+    if (!CellContainsObstacle(mapData, gridX, gridY)) {
+        // Update the player's position only if the cell doesn't contain an obstacle
+        *Player = nextPosition;
+    }
+}
+
+void ControlsInitControls(Vector2* Player, GridData* mapData)
+{
+    HandlePlayerControls(Player, mapData);
+}
+
+
+
+
